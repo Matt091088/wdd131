2547bb9e42377426e1665659f95f7c0b404e4a8d
mac <macbookpro@MacBook-Pro-de-mac.local>
2025-07-19 21:26:13 -0300

    Fix mobile SVG icon visibility

--- week3/scripts/place.js
+++ week3/scripts/place.js
@@ -1,7 +1,10 @@
-
-
 function calculateWindChill(tempC, windKmh) {
-  return (13.12 + 0.6215 * tempC - 11.37 * Math.pow(windKmh, 0.16) + 0.3965 * tempC * Math.pow(windKmh, 0.16)).toFixed(1);
+  return (
+    13.12 +
+    0.6215 * tempC -
+    11.37 * Math.pow(windKmh, 0.16) +
+    0.3965 * tempC * Math.pow(windKmh, 0.16)
+  ).toFixed(1);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const windElem = document.getElementById('wind');
   const windChillElem = document.getElementById('windchill');
 
-  const temp = parseFloat(tempElem.textContent);
-  const wind = parseFloat(windElem.textContent);
+  const temp = parseFloat(tempElem?.textContent || 0);
+  const wind = parseFloat(windElem?.textContent || 0);
 
   if (temp <= 10 && wind > 4.8) {
     windChillElem.textContent = calculateWindChill(temp, wind) + ' °C';
@@ -19,9 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Actualizar footer año y última modificación
-  const yearSpan = document.getElementById("year");
+  const yearSpan = document.getElementById('year');
   if (yearSpan) yearSpan.textContent = new Date().getFullYear();
 
-  const lastModifiedSpan = document.getElementById("lastModified");
+  const lastModifiedSpan = document.getElementById('lastModified');
   if (lastModifiedSpan) lastModifiedSpan.textContent = document.lastModified;
 });
